9d0490b661d57371adf90af62129ba67672ead6b
Abhishek-singh <abhisheksinghofficial89@gmail.com>
2022-04-15 20:52:14 +0530

    creditcard

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CardForm from './component/CardForm';
+
+function App() {
+  return (
+    <div className="App">
+     <CardForm/>
+    </div>
+  );
+}
+
+export default App;
